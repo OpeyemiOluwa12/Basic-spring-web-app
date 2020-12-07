b0a35a35e84326b6ad71f98d5ac6d640f588c56a
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-12-07 13:20:06 +0100

    Add model to home controller

--- src/main/java/com/opeyemi/spring/basicwebapp/controller/HomeController.java
+++ src/main/java/com/opeyemi/spring/basicwebapp/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.opeyemi.spring.basicwebapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Instant;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String getHomePage(){
+    public String getHomePage(Model model) {
+        model.addAttribute("welcomeMessage", "Hi Welcome"+ Instant.now().toString());
         return "home";
     }
 }
