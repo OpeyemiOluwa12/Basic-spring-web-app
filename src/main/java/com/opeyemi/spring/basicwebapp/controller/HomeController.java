ea998dc0466ab87fa8ae6a0ea4001b290d863cae
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-12-10 07:05:31 +0100

    Bind a post mapping to form submit

--- src/main/java/com/opeyemi/spring/basicwebapp/controller/HomeController.java
+++ src/main/java/com/opeyemi/spring/basicwebapp/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.opeyemi.spring.basicwebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.Instant;
@@ -9,9 +12,23 @@ import java.time.Instant;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String getHomePage(Model model) {
-        model.addAttribute("welcomeMessage", "Hi Welcome"+ Instant.now().toString());
+    private MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        model.addAttribute("greetings", this.messageListService.getMessages());
+        return "home";
+    }
+
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute("newMessage") MessageForm messageForm, Model model) {
+        messageListService.addMessage(messageForm.getText());
+        model.addAttribute("greetings", messageListService.getMessages());
+        messageForm.setText("");
         return "home";
     }
 }
