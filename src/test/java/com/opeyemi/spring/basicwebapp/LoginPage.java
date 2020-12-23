6795071bc1235d76aec6d0aa606144a1b01bdff8
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-12-23 23:52:09 +0100

    Write selenium test for the chat application

---
+++ src/test/java/com/opeyemi/spring/basicwebapp/LoginPage.java
@@ -0,0 +1,33 @@
+package com.opeyemi.spring.basicwebapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "inputUsername")
+    private WebElement usernameField;
+
+    @FindBy(id = "inputPassword")
+    private WebElement passwordField;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+    @FindBy(id = "error-msg")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver webDriver){
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void login(String username, String password){
+        this.usernameField.sendKeys(username);
+        this.passwordField.sendKeys(password);
+        submitButton.click();
+    }
+
+
+}
